bf99e1850bfd22fc664a2856701ba69da9c789d7
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-09-10 22:43:25 +0300

    ra-composition-cards-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Card from "./react-component/Card";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card
+        title={"Card title"}
+        text={"Some quick example text to build on the card title and make up the bulk of the card's content."}
+      >
+        <img
+          alt={"Картинка-заглушка"}
+          src={"https://oboitut.com/uploads/posts/2016-05/thumbs/_oboitut.com_1462713283.jpg"}
+        />
+      </Card>
+      <Card
+        title={"Special title treatment"}
+        text={"With supporting text below as a natural lead-in to additional content."}
+      />
     </div>
   );
 }
